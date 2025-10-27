2fc6e494a62e7bcaa9fd00213cc98ebcb2a0e7af
Uzma Shaikh <uzmashxikh@hotmail.com>
2025-10-26 21:17:15 -0400

    ffully edited UI Main,About,AddEdit,Splash and Details

--- app/src/main/java/ca/gbc/restaurantguide/ui/DetailsActivity.kt
+++ app/src/main/java/ca/gbc/restaurantguide/ui/DetailsActivity.kt
@@ -10,11 +10,10 @@ class DetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState); setContentView(R.layout.activity_details)
-//        setSupportActionBar(binding.topBar)
-//        supportActionBar?.title = "Restaurant Details"
-//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//
-//        binding.topBar.setNavigationOnClickListener { finish() }
+        setSupportActionBar(binding.topBar)
+        supportActionBar?.title = "Restaurant Details"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+//        binding.topBar.setNavigationOnClickListener { finish() }
     }
 }
